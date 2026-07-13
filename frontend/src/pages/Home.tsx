186a7a039caffae9ddeb9646a234ed238ec91693
Villarreal Cardenas Jonathan Rolando <jhon_villac@hotmail.com>
2026-07-12 21:13:02 -0500

    settionDelete

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,6 @@ import Header from '../components/layout/Header'
 import Footer from '../components/layout/Footer'
 
 import HeroSection from '../components/home/HeroSection'
-import CTASection from '../components/home/CTASection'
 
 export default function Home() {
   return (
@@ -11,7 +10,6 @@ export default function Home() {
 
       <main>
         <HeroSection />
-        <CTASection />
       </main>
 
       <Footer />
